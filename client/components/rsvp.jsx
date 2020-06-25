9572e475b75bc0fb9da23b688c610aae908d9186
Nathan Reitan <nathanreitan@gmail.com>
2020-06-25 16:49:19 -0700

    form restructured and formatted

--- client/components/rsvp.jsx
+++ client/components/rsvp.jsx
@@ -30,20 +30,61 @@ export default class Rsvp extends React.Component {
   render() {
     return (
       <div>
-        <form action="">
-          <label htmlFor="name">Name:</label>
-          <input type="text" id='name' name='name' placeholder='First and Last Name'/>
-          <label htmlFor="plusOne">Plus One:</label>
-          <input type="radio" id='plusOne' name='plusOne' value='yes'/>
-          <input type="radio" id='plusOne' name='plusOne' value='no'/>
-          <input type="text" id='plusOneName' name='plusOneName' placeholder='First and Last Name of Guest'/>
-          <label htmlFor="mealOptions">Meal Options:</label>
-          <ol>
-            <li>Steak</li>
-            <li>Fish</li>
-            <li>Vegetarian</li>
-          </ol>
-          <input type="text" id='mealOptions' name='mealOptions' placeholder="enter meals here(if you have a plus one and don't enter two meals it will be assumed you both want the same meal)"/>
+        <form action="" className='d-flex flex-wrap justify-content-end px-1'>
+          <div className='w-100'>
+            <h4>Guest 1:</h4>
+            <div className='w-100'>
+              <label htmlFor="name" className='w-50'>Name</label>
+              <input type="text" id='name' name='name' placeholder='First and Last Name'/>
+            </div>
+          </div>
+          <div className='w-100'>
+            <h4>Meal Options:</h4>
+            <div className='w-75 mx-2'>
+              <div>
+                <label htmlFor="steak" className='w-50'>Steak</label>
+                <input type="radio" name='mealOption1' id='steak' value='steak'/>
+              </div>
+              <div>
+                <label htmlFor="fish" className='w-50'>Fish</label>
+                <input type="radio" name='mealOption1' id='fish' value='fish'/>
+              </div>
+              <div>
+                <label htmlFor="vegetarian" className='w-50'>Vegetarian</label>
+                <input type="radio" name='mealOption1' id='vegetarian' value='vegetarian'/>
+              </div>
+            </div>
+            <div className='w-100'>
+              <label htmlFor="allergies1" className='w-50'>Allergies</label>
+              <input type="text" name="allergies" id="allergies1" placeholder='enter any allergies here' />
+            </div>
+            <h4>Guest 2:</h4>
+            <div className='w-100'>
+              <label htmlFor="name" className='w-50'>Name</label>
+              <input type="text" id='name' name='name' placeholder='First and Last Name' />
+            </div>
+          </div>
+          <div className='w-100'>
+            <h4>Meal Options:</h4>
+            <div className='w-75 mx-2'>
+              <div>
+                <label htmlFor="steak" className='w-50'>Steak</label>
+                <input type="radio" name='mealOption2' id='steak' value='steak' />
+              </div>
+              <div>
+                <label htmlFor="fish" className='w-50'>Fish</label>
+                <input type="radio" name='mealOption2' id='fish' value='fish' />
+              </div>
+              <div>
+                <label htmlFor="vegetarian" className='w-50'>Vegetarian</label>
+                <input type="radio" name='mealOption2' id='vegetarian' value='vegetarian' />
+              </div>
+            </div>
+            <div className='w-100'>
+              <label htmlFor="allergies2" className='w-50'>Allergies</label>
+              <input type="text" name="allergies" id="allergies2" placeholder='enter any allergies here' />
+            </div>
+          </div>
           <input type="submit" value='submit'/>
         </form>
       </div>
