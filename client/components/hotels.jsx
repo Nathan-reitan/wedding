6ea4ba3a7e8731195c9e98ba3027a9e39720b3ec
Nathan Reitan <nathanreitan@gmail.com>
2020-07-24 12:49:58 -0700

    list setup

--- client/components/hotels.jsx
+++ client/components/hotels.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 export default function Hotels(props) {
   return (
-    <main>
+    <div>
+      <h1>Hotels</h1>
+      <div>
+      We have taken the time to locate some hotels in the area, please see the list below:
+      </div>
+      <ol>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ol>
+    </div>
 
-    </main>
   );
 }
