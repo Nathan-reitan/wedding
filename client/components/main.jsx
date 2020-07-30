ae29ab9c03394dc27ccc91e495dd992a3a687837
Nathan Reitan <nathanreitan@gmail.com>
2020-07-30 13:35:30 -0700

    margin changes to Our Story page, on paragraph elements

--- client/components/main.jsx
+++ client/components/main.jsx
@@ -6,20 +6,20 @@ export default function Main(props) {
       <div className='kaushan'>
         <h5 className='berkshire'>Our Story</h5>
         <div>
-          <p>
+          <p className='my-2'>
             Nathan and Mackenzie met in 2014 and went on their first date to Knottfest.
           </p>
           <div>
             <img className='w-100'src="./images/knottFest.jpg" alt="KnottFest"/>
           </div>
-          <p>
+          <p className='my-2'>
             Their love for music helped jump start their loving relationship.
             Eventually they moved off to Montana together, where Mackenzie attended the University of Montana.
           </p>
           <div>
             <img className='w-100' src="./images/mtWaterfall.jpg" alt="Montana Waterfall"/>
           </div>
-          <p>
+          <p className='my-2'>
             While in Montana, Nathan decided that he would (finally, according to Kenzie) pop the question and go get her a ring.
             They planned a camping trip right outside of Yellowstone and on the way down they hiked to a waterfall where he asked her.
           </p>
@@ -27,7 +27,7 @@ export default function Main(props) {
             <img className='w-100' src="./images/proposal.jpg" alt="Proposal"/>
             <p className='text-center'><em>(Thanks to the random lady who took the photo.)</em></p>
           </div>
-          <p>
+          <p className='my-2'>
             Now they look forward to spending the rest of their lives together and building a prosperous life together.
           </p>
         </div>
